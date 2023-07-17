19be807c24296c6491806c5edc535c4569d7ee91
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-07-17 18:47:25 -0400

    Carousel Working Great Now - rendering images along with their appropriate background colours - will refactor code soon after

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,25 +1,47 @@
 <script setup lang="ts">
 import type { Ref } from 'vue'
-import { ref, computed, onMounted } from "vue";
+import { ref, reactive, computed, onMounted } from "vue";
 import { usePokéStore } from "../store/pokemon";
 import ColorThief from "colorthief";
 
 const store = usePokéStore();
-
-const x = ref({});
-
+const pokémon = ref({});
+const img: Ref<HTMLImageElement | null> = ref(null);
+const palette = reactive({
+  primary: "",
+  secondary: "",
+});
 
 //@ts-ignore
 await store.fetchPokémon();
+const getColours = () => {
+  //@ts-ignore
+  const colorThief = new ColorThief();
+
+  if (img.value?.complete) colorThief.getColor(img.value);
+  console.log(colorThief.getPalette(img.value));
+  const colourPalette = colorThief.getPalette(img.value);
+  const red = colourPalette[0][0];
+  const green = colourPalette[0][1];
+  const blue = colourPalette[0][2];
+  const rouge = colourPalette[1][0];
+  const vert = colourPalette[1][1];
+  const bleu = colourPalette[1][2];
+  // ==========================
+  palette.primary = `rgb(${red},${green},${blue})`;
+  palette.secondary = `rgb(${rouge},${vert},${bleu})`;
+  console.log(palette.primary);
+
+}
 
 onMounted(() => {
   store.randomisePokémon();
-  x.value = store.randomisePokémon();
-  console.log(x.value);
+  pokémon.value = store.randomisePokémon();
+  console.log(pokémon.value);
   setInterval(() => {
-    x.value = store.randomisePokémon();
+    pokémon.value = store.randomisePokémon();
     console.log("Fetching random Pokemon");
-    console.log(x.value);
+    console.log(pokémon.value);
   }, 3000)
 });
 
@@ -27,8 +49,18 @@ onMounted(() => {
 </script>
         
 <template>
-  <figure v-if="store.pokémon.length !== 0" class="container" ref="figure">
-    <img :src="x.sprites?.other['official-artwork'].front_default" crossorigin="anonymous" ref="img" />
+  <figure class="container" :style="{
+    backgroundImage: `linear-gradient(
+            to right,
+            ${palette.primary} 0%,
+                ${palette.primary} 70%,
+                ${palette.secondary} 70%,
+                ${palette.secondary} 100%
+        )` }">
+    <!-- commented out code ne marche pas. celui sans la point d'interrogation -->
+    <!-- <img :src="pokémon.sprites.other['official-artwork'].front_default" crossorigin="anonymous" ref="img" /> -->
+    <img @load="getColours" :src="pokémon.sprites?.other['official-artwork'].front_default" crossorigin="anonymous"
+      ref="img" />
   </figure>
   <!-- <span>{{ x.sprites.other['official-artwork'].front_default }}</span> -->
 </template>

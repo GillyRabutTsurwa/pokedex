10e5abffe81f24baa1d6b1bdc8689f1096dd7a1b
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-07-18 22:15:18 -0400

    Starting Development of Pokémon List Page

---
+++ pages/pokemon/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { usePokéStore } from '../../store/pokemon';
+const store = usePokéStore();
+await store.fetchPokémon();
+</script>
+
+<template>
+    <div>
+        <h4>Pokémon Page</h4>
+        <ul>
+            <li v-for="{ name } in store.pokémon" :key="{ id }">
+                {{ name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<style scoped></style>

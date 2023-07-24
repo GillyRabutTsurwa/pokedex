f97e94c5107d14d55f54e70e0bd779fc7a074524
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-07-24 12:58:53 -0400

    I think I know Why the ColorThief Composable Was Not Working - this because i am not properly targetting the image element based on how i wrote the code - i will try test this out and see if that was indeed the problem

--- components/Pokepage.vue
+++ components/Pokepage.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import ColorThief from "colorthief";
+
 // TESTING: copied from store file
 type PokéStripe = {
   front_default: string;
@@ -21,11 +23,10 @@ type PokéStat = {
 interface Pokémon {
   name: string;
   id: number;
-  abilities: Array<{ name: string }>;
-  moves: Array<{ move: { name: string; url: string } }>;
-  sprites: PokéStripe;
-  stats: Array<PokéStat>;
+  img: string;
   types: Array<{ type: { name: string } }>;
+  abilities: Array<{ name: string }>;
+  stats: Array<PokéStat>;
 }
 
 // props interface
@@ -34,13 +35,56 @@ interface Props {
 }
 
 const props = defineProps<Props>();
-console.log(props.pokémon);
+
+interface Palette {
+  primary: string;
+  secondary: string;
+}
+
+const palette: Palette = reactive({
+  primary: "",
+  secondary: "",
+});
+
+const getColours: () => void = (): void => {
+  const colorThief = new ColorThief();
+
+  if (img.value?.complete) colorThief.getColor(img.value);
+  const colourPalette = colorThief.getPalette(img.value);
+  const red = colourPalette[0][0];
+  const green = colourPalette[0][1];
+  const blue = colourPalette[0][2];
+  const rouge = colourPalette[1][0];
+  const vert = colourPalette[1][1];
+  const bleu = colourPalette[1][2];
+
+  palette.primary = `rgb(${red},${green},${blue})`;
+  palette.secondary = `rgb(${rouge},${vert},${bleu})`;
+};
+
+const img: Ref<HTMLImageElement | null> = ref(null);
+const fetchImageColours = () => {
+  getColours();
+}
+const bg = computed(() => {
+  return {
+    backgroundColor: palette.secondary
+  }
+});
+console.log(palette);
 </script>
 
 <template>
-  <div>
+  <div class="container" :style="bg">
     <h4>{{ props.pokémon.name }}</h4>
+    <p>{{ props.pokémon.types }}</p>
+    <img @load="fetchImageColours" :src="props.pokémon.img" ref="img" crossorigin="anonymous">
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  width: 100%;
+  height: inherit;
+}
+</style>
--- composables/colours.ts
+++ composables/colours.ts
@@ -13,12 +13,11 @@ export const useColours = () => {
     });
     const tableau: Ref<string[]> = ref([]);
     const tableau2: Ref<string[]> = ref([]);
-    // const test = () => console.log("composable test");
+
     const getColours: (image: any) => void = (imageEl): void => {
         const colorThief = new ColorThief();
 
         if (imageEl.currentTarget?.complete && imageEl.currentTarget.naturalWidth > 0) colorThief.getColor(imageEl.currentTarget);
-        // console.log(colorThief.getPalette(payload.currentTarget));
         const colourPalette = colorThief.getPalette(imageEl.currentTarget);
         const red = colourPalette[0][0];
         const green = colourPalette[0][1];
@@ -26,17 +25,14 @@ export const useColours = () => {
         const rouge = colourPalette[1][0];
         const vert = colourPalette[1][1];
         const bleu = colourPalette[1][2];
-        // ==========================
+
         palette.primary = `rgb(${red},${green},${blue})`;
         palette.secondary = `rgb(${rouge},${vert},${bleu})`;
 
-        console.log(`rgb(${red},${green},${blue})`);
         tableau.value.push(`rgb(${red},${green},${blue})`);
         tableau2.value.push(`rgb(${rouge},${vert},${bleu})`);
-        console.log(tableau.value);
     };
     return {
-        // test: test,
         getColours: getColours,
         palette: palette,
         tableau: tableau,
